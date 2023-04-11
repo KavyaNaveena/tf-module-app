b6f056373cb5082935e6231d5622a063409c0871
Kavya Naveena <karumurikavya@gmail.com>
2023-04-11 15:44:31 +0530

    update

--- vars.tf
+++ vars.tf
@@ -2,3 +2,7 @@ variable "component" {}
 variable "env" {}
 variable "tags" {}
 variable "instance_type" {}
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
+variable "subnets" {}
